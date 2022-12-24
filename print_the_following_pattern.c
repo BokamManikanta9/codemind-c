7cc69816c03f0c89587b93b482e670b70bd3e989
BokamManikanta9 <119719201+BokamManikanta9@users.noreply.github.com>
2022-12-24 10:52:03 +0530

    #include<stdio.h> int main() {     int n,i,j,m;     scanf("%d",&n);        m=n-2;											//12312     for(i=1;i<=n;i++)									//12312     {													//12312        													//12312         for(j=1;j<=m;j++)								//12312         {             printf("%d",j);         }												//121      													//121         for(j=m-1;j>=1;j--)								//121         {             printf("%d",j);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,16 +3,16 @@ int main()
 {
     int n,i,j,m;
     scanf("%d",&n);
-       m=n-2;
-    for(i=1;i<=n;i++)
-    {
-       
-        for(j=1;j<=m;j++)
+       m=n-2;											//12312
+    for(i=1;i<=n;i++)									//12312
+    {													//12312
+       													//12312
+        for(j=1;j<=m;j++)								//12312
         {
             printf("%d",j);
-        }
-     
-        for(j=1;j<m;j++)
+        }												//121
+     													//121
+        for(j=m-1;j>=1;j--)								//121
         {
             printf("%d",j);
         }
