21310e6e2aca49532fda441900c1e281a7a22b4d
BokamManikanta9 <119719201+BokamManikanta9@users.noreply.github.com>
2023-01-30 21:28:45 +0530

    #include<stdio.h> int main() {     int n,i,j,e=0,c;     scanf("%d",&n);     float a[n],avg=0;     for(i=0;i<n;i++)     {         scanf("%f",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(a[i]==a[j])             {                 c++;                 if(i!=j)                 {                     a[j]=0;                 }             }         }         if(c==a[i])         { avg=avg+a[i]; e++;         }     }     if(e>0)     {         avg=avg/e;         printf("%.2f",avg);     }     else     {         printf("-1");     } }

---
+++ Average_of_super_elements.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,e=0,c;
+    scanf("%d",&n);
+    float a[n],avg=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%f",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                c++;
+                if(i!=j)
+                {
+                    a[j]=0;
+                }
+            }
+        }
+        if(c==a[i])
+        {
+avg=avg+a[i];
+e++;
+        }
+    }
+    if(e>0)
+    {
+        avg=avg/e;
+        printf("%.2f",avg);
+    }
+    else
+    {
+        printf("-1");
+    }
+}
